99f433d0bf2456892aed0e4648a61e98fbdf795a
kevin321 <kevin321vip@126.com>
2018-03-03 10:30:11 +0800

    updata

--- app/src/main/java/com/wh/jxd/com/mvvmdemo/StudentActivity.java
+++ app/src/main/java/com/wh/jxd/com/mvvmdemo/StudentActivity.java
@@ -2,23 +2,25 @@ package com.wh.jxd.com.mvvmdemo;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import com.android.databinding.library.baseAdapters.BR;
 import com.wh.jxd.com.mvvmdemo.bean.User;
 
-public class MainActivity extends AppCompatActivity {
+public class StudentActivity extends AppCompatActivity {
     private ViewDataBinding mBinding;
     private User mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        mUser = new User("老疼", "26");
-        mBinding.setVariable(BR.user, mUser);
-//        mBinding.
-//      mBinding.setVariable(BR);
-        mUser.setName("哈哈哈哈哈");
+        ViewDataBinding viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_student);
+//        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        mUser = new User("老疼", "26");
+//        mBinding.setVariable(BR.user, mUser);
+////        mBinding.
+////      mBinding.setVariable(BR);
+//        mUser.setName("哈哈哈哈哈");
 
     }
 
